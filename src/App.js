123c94bea2612ebb9e915544dd0b521ff8a2faa2
Hrishikesh Deka <63395766+hrishikeshdeka@users.noreply.github.com>
2022-02-02 22:25:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import TodoApp from './components/TodoApp';
+
+function App() {
+  return (
+    <div className="container-fluid todoApp">
+      <div className="row">
+        <h1 className="text-center font-weight-lighter display-6 col-md-12 py-5 text-uppercase text-primary">
+          Simple todo app
+        </h1>
+        <TodoApp />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
